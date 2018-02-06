c64612d8b7cfe05586ba5eecb7b233a36b0a6181
Denisacademy <brokenhandq3@yandex.ru>
2018-02-07 00:12:00 +0300

    remarks

--- js/map.js
+++ js/map.js
@@ -6,40 +6,41 @@ var checkin = ['12:00', '13:00', '14:00'];
 var checkout = ['12:00', '13:00', '14:00'];
 var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 
-function randomNum(num) {
+function getRandomNumber(num) {
   return 1 + Math.floor(Math.random() * num);
 }
 
-function randomHorizon(min, max) {
+function getRandomBetween(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
-function genObj(num) {
-  var createMass = [];
+function fillOffersData(num) {
+  var offers = [];
 
   for (var i = 0; i < num; i++) {
 
-    var x = randomHorizon(300, 900);
-    var y = randomHorizon(150, 500);
+    var x = getRandomBetween(300, 900);
+    var y = getRandomBetween(150, 500);
 
-    var zab = [];
-    zab[num] = features.slice(0, randomNum(5));
-
-    createMass[i] = {
+    var featureCount = [];
+	featureCount = features.sort(function(a,b) { return Math.random() -0.5 });
+	featureCount = featureCount.slice(0, getRandomNumber(5));
+        
+	offers.push({
       'author': {
         'avatar': 'img/avatars/user0' + (i + 1) + '.png',
-        'price': randomHorizon(100, 200)
+        'price': getRandomBetween(100, 200)
       },
       'offer': {
         'title': title[i],
         'address': 'location.' + x + " " + 'location.' + y,
-        'price': randomHorizon(1000, 1000000),
-        'type': type[randomHorizon(0, 2)],
-        'rooms': randomNum(5),
-        'guests': randomNum(10),
-        'checkin': checkin[randomHorizon(0, 2)],
-        'checkout': checkout[randomHorizon(0, 2)],
-        'features': zab[num],
+        'price': getRandomBetween(1000, 1000000),
+        'type': type[getRandomBetween(0, 2)],
+        'rooms': getRandomNumber(5),
+        'guests': getRandomNumber(10),
+        'checkin': checkin[getRandomBetween(0, 2)],
+        'checkout': checkout[getRandomBetween(0, 2)],
+        'features': featureCount,
         'description': " ",
         'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg']
       },
@@ -47,9 +48,10 @@ function genObj(num) {
         'x': x,
         'y': y
       }
-    }
+    })
   }
-  return createMass;
+  console.log(offers);
+  return offers;
 }
 
 
@@ -63,12 +65,12 @@ switchMap();
 
 //Task 3
 
-var makepins = genObj(8);
+var makepins = fillOffersData(3);
 
 function Makepin() {
-  //var makepins = genObj(8);
+  //var makepins = fillOffersData(8);
   var documentFragment = document.createDocumentFragment();
-  
+
   for (var i = 0; i < makepins.length; i++) {
     var button = document.createElement('button');
     button.style.left = makepins[i].location.x + "px";
@@ -80,14 +82,14 @@ function Makepin() {
     img.style.width = "40px";
     img.style.height = "40px";
     img.style.draggable = "false";
-	
-	button.appendChild(img);
-	button.appendChild(img);
+
+        button.appendChild(img);
+        button.appendChild(img);
     documentFragment.appendChild(button);
    }
    //console.log(documentFragment);
   return documentFragment;
-  
+
   //return button;
 }
 
@@ -95,9 +97,9 @@ function Makepin() {
 
 
 function renderPins() {
-	var mapPins = document.querySelector('.map__pins');
-	var donePins =  Makepin();
-	mapPins.appendChild(donePins);
+        var mapPins = document.querySelector('.map__pins');
+        var donePins =  Makepin();
+        mapPins.appendChild(donePins);
 }
 
 renderPins();
@@ -116,10 +118,10 @@ function typeRoom(type) {
 
 function fillCard() {
 	var template = document.querySelector('template');
-	var post = template.content;
-	
-	post.cloneNode(true);
-	
+	var post = template.content.querySelector('.map__card').cloneNode(true);
+	//var template = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
+
+	//console.log(post);
 	
 	
 	var title = post.querySelector('h3');
@@ -128,23 +130,71 @@ function fillCard() {
 	var type = post.querySelector('h4');
 
 
-	title.textContent = makepins[0].offer.title;
-	address.textContent = makepins[0].offer.address;
-	price.textContent = makepins[0].offer.price + " &#x20bd;/ночь";
-	type.textContent = makepins[0].offer.type;
+	//title.textContent = makepins[0].offer.title;
+	//address.textContent = makepins[0].offer.address;
+	//price.textContent = makepins[0].offer.price + " &#x20bd;/ночь";
+	//type.textContent = makepins[0].offer.type;
 
-	var divtest = document.createElement("div");
-	var textDiv = document.createTextNode("Hello world");
-	divtest.appendChild(textDiv);
+	//var divtest = document.createElement("div");
+	//var textDiv = document.createTextNode("Hello world");
+	//divtest.appendChild(textDiv);
 	
 	
 	var mapPins = document.querySelector('.map__pins');	
 	var map__filters = document.querySelector('.map__filters-container');
-	map__filters.before(mapPins, divtest);
+	map__filters.before(mapPins, post);
 	//map__filters.insertAdjacentHTML('beforebegin', divtest);
 }
 
 fillCard();
+
+/*
+//5555 
+
+//Task 5
+
+function typeRoom(type) {
+        switch (type) {
+                case flat: 'Квартира'; 
+                case house:  'Дом'; 
+                case bungalo: 'Бунгало';  
+        }
+}
+
+
+
+function fillCard() {
+        var template = document.querySelector('template');
+        var post = template.content;
+
+        post.cloneNode(true);
+
+
+
+        var title = post.querySelector('h3');
+        var address = post.querySelector('p small');
+        var price = post.querySelector('.popup__price');
+        var type = post.querySelector('h4');
+
+
+        title.textContent = makepins[0].offer.title;
+        address.textContent = makepins[0].offer.address;
+        price.textContent = makepins[0].offer.price + " &#x20bd;/ночь";
+        type.textContent = makepins[0].offer.type;
+
+        var divtest = document.createElement("div");
+        var textDiv = document.createTextNode("Hello world");
+        divtest.appendChild(textDiv);
+
+
+        var mapPins = document.querySelector('.map__pins');	
+        var map__filters = document.querySelector('.map__filters-container');
+        map__filters.before(mapPins, divtest);
+        //map__filters.insertAdjacentHTML('beforebegin', divtest);
+}
+
+fillCard();
+*/
 
 
 
